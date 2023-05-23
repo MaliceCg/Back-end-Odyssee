506c6b6d814e40585f7292b59f0e83b70f273a6d
Marine <marine.cantaloube-girona@etu.montpellier.fr>
2023-05-23 21:27:08 +0200

    add origin cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-  origin: ['https://ody.onrender.com'],
+  origin: ['https://ody.onrender.com', 'http://127.0.0.1:5500'],
   credentials: true
 }
 app.use(cors(corsOptions));
